57a11e105bbefbfd6123bba099832bb27293b30e
Anjali-Deshpande <anjali.deshpande10@gmail.com>
2017-03-16 12:38:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,3 +35,31 @@ app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
+
+// to run a query we can acquire a client from the pool,
+// run a query on the client, and then return the client to the pool
+pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * from Student', ['1'], function(err, result) {
+    //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+    done(err);
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0]);
+    //output: 1
+  });
+});
+
+pool.on('error', function (err, client) {
+  // if an error is encountered by a client while it sits idle in the pool
+  // the pool itself will emit an error event with both the error and
+  // the client which emitted the original error
+  // this is a rare occurrence but can happen if there is a network partition
+  // between your application and the database, the database restarts, etc.
+  // and so you might want to handle it and at least log it out
+  console.error('idle client error', err.message, err.stack)
+})
